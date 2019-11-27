b6846abff4c7b1f6060847db59a9500cd294b75e
gouf <innocent.zero@gmail.com>
2019-11-27 13:51:54 +0900

    Initial Commit

---
+++ Bullseye/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Bullseye
+//
+//  Created by 古屋郷 on 2019/11/27.
+//  Copyright © 2019 古屋郷. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
